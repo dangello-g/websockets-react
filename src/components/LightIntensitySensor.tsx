cd30d310738f768f7469f9d811dc989939e64041
dangello-g <garcia.dangello@gmail.com>
2025-02-26 00:26:24 -0500

    feat: WebSocket reconnection implemented

--- src/components/LightIntensitySensor.tsx
+++ src/components/LightIntensitySensor.tsx
@@ -1,15 +1,20 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Sun } from "lucide-react"
 import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 import { useEffect, useState } from "react";
-import { SensorProps } from "../App";
+import { SensorProps } from "../types/sensors";
 
-export default function LightIntensitySensor({ value, timestamp }: SensorProps) {
+export default function LightIntensitySensor({ value, timestamp, onThresholdAlert }: SensorProps) {
     const [data, setData] = useState<{ value: number, time: string }[]>([]);
+    const threshold = 1100;
 
     useEffect(() => {
         setData((prevData) => [...prevData, { value, time: new Date(timestamp).toLocaleTimeString() }].slice(-10));
     }, [value, timestamp]);
+
+    const handleThresholdAlert = () => {
+        onThresholdAlert(value);
+    };
 
     return (
         <>
@@ -32,6 +37,7 @@ export default function LightIntensitySensor({ value, timestamp }: SensorProps) 
                             </LineChart>
                         </ResponsiveContainer>
                     </div>
+                    <Button className="mt-4 w-100" onClick={handleThresholdAlert} disabled={value <= threshold}>{value > threshold ? `Alert: High Light Intensity at ${new Date(timestamp).toLocaleTimeString()}` : `Threshold: ${threshold}`}</Button>
                 </Card.Body>
             </Card>
 
